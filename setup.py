c17b23f0f14a87a2581ad0dff1cbf3be2f871958
Manish <mky80094@gmail.com>
2025-09-02 23:41:48 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+setup(
+    name = 'Gen AI Project',
+    version='0.0.0',
+    author='Masss',
+    author_email='mky80094@gmail.com',
+    packages=find_packages(),
+    install_requires = []
+)
